999d72474771549d015f65b1176190b515467bed
Krish Pawar <pawarkrish641@gmail.com>
2026-07-20 22:54:14 +0530

    Add solution: Maximum Count of Positive Integer and Negative Integer (Easy)

---
+++ Array/maximum-count-of-positive-integer-and-negative-integer/Solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int n = nums.length;
+
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        int negative = low;
+
+        low = 0;
+        high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] <= 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        int positive = n - low;
+
+        return Math.max(negative, positive);
+    }
+}
