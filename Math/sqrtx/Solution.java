b7f50aa338474c73e7cb55b39c92fea1002772cd
Krish Pawar <pawarkrish641@gmail.com>
2026-07-22 23:02:10 +0530

    Update solution: Sqrt(x) (Easy)

--- Math/sqrtx/Solution.java
+++ Math/sqrtx/Solution.java
@@ -1,10 +1,20 @@
 class Solution {
-    public int mySqrt(int x) {
-       int root=0;
-        for(int i=1;i<=x;i++){
-            if((long)i*i > x)  break;
-            root = i;          
+    public int mySqrt(int n) {
+       if(n==0) return 0;
+       int low=1;
+       int high=n;
+       while(low<=high){
+        int mid=low+(high-low)/2;
+        if(mid==n/mid){
+            return mid;
         }
-        return root;
+        else if(mid < n/mid){
+            low = mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+       }
+       return high;
     }
 }
