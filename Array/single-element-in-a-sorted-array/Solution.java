d22780dea1351af7a04e2af7903c579333907f3f
Krish Pawar <pawarkrish641@gmail.com>
2026-07-24 00:25:54 +0530

    Update solution: Single Element in a Sorted Array (Medium)

--- Array/single-element-in-a-sorted-array/Solution.java
+++ Array/single-element-in-a-sorted-array/Solution.java
@@ -1,22 +1,23 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int low = 0;
-        int high = nums.length - 1;
-
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (mid % 2 == 1) {
-                mid--;
-            }
-
-            if (nums[mid] == nums[mid + 1]) {
-                low = mid + 2;
-            } else {
-                high = mid;
-            }
+        int n=nums.length;
+        if(n==1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]) return nums[mid];
+            int s=mid;
+            int f=mid;
+            if(nums[mid] == nums[mid+1]) s=mid+1;
+            else if(nums[mid] == nums[mid-1]) f=mid-1;
+            int leftcount = f-low;
+            int rightcount = high - s;
+            if(leftcount%2==0) low = s+1;
+            else high=f-1;
         }
-
-        return nums[low];
+        return 0;
     }
 }
