28132a77dd43572558546bb2e89fa112aeebedef
Krish Pawar <pawarkrish641@gmail.com>
2026-08-04 11:43:25 +0530

    Update solution: Missing Number (Easy)

--- Array/missing-number/Solution.java
+++ Array/missing-number/Solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int sum = 0;
-        int totalSum = n*(n+1)/2;
+        int sum = n*(n+1)/2;
+        int arraysum=0;
         for(int i=0;i<n;i++){
-            sum += nums[i];
+            arraysum += nums[i];
         }
-        int missing = totalSum - sum;
+        int missing=sum-arraysum;
         return missing;
     }
 }
