e54d197d14b30393ee37c2b01f81f09461bbb84b
Krish Pawar <pawarkrish641@gmail.com>
2026-08-04 11:25:06 +0530

    Update solution: Binary Search (Easy)

--- Array/binary-search/Solution.java
+++ Array/binary-search/Solution.java
@@ -1,20 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
-        int high=n-1;
         int low=0;
+        int high=nums.length-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]<target){
-                low=mid+1;
+            int mid=low+(high-low)/2;
+
+            if(nums[mid] < target){
+                low = mid + 1;
             }
-            else if(nums[mid]>target){
-                high=mid-1;
+            else if(nums[mid] > target){
+                high= mid-1;               
             }
             else{
-                return mid;
+                 return mid;
             }
         }
-        return -1;
+      return -1;
     }
 }
