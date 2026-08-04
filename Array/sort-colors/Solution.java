25e4e5fccf77bbf3b62e8fc6fa3444f7b00e46e3
Krish Pawar <pawarkrish641@gmail.com>
2026-08-04 11:57:41 +0530

    Update solution: Sort Colors (Medium)

--- Array/sort-colors/Solution.java
+++ Array/sort-colors/Solution.java
@@ -1,36 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-    int numofzeros=0;
-    int numofones=0;
-    int numoftwos=0;
-    for(int ele : nums){
-        if(ele == 0){
-            numofzeros++;
+        int numoftwo=0;
+        int numofzeros=0;
+        int numofones=0;
+        int n=nums.length;
+        for(int ele : nums){
+            if(ele==0) numofzeros++;
+            else if(ele==1) numofones++;
+            else numoftwo++;
         }
-        else if(ele == 1){
-            numofones++;
-        } else {
-            numoftwos++;
+        int i=0;
+        while(numofzeros > 0){
+            nums[i]=0;
+            numofzeros--;
+            i++;
         }
-            
+        while(numofones > 0){
+            nums[i]=1;
+            numofones--;
+            i++;
         }
-    int i=0;
-    while (numofzeros > 0) {
-        nums[i]=0;
-        i++;
-        numofzeros--;
+        while(numoftwo > 0){
+            nums[i]=2;
+            numoftwo--;
+            i++;
         }
-     while (numofones > 0) {
-        nums[i]=1;
-        i++;
-        numofones--;
-        }
-    while (numoftwos > 0) {
-        nums[i]=2;
-        i++;
-        numoftwos--;
-        }
-
     }
-
 }
