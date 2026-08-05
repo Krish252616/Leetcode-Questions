7059e2a5d3b9ddc52cbf5a02013ad53c811743bf
Krish Pawar <pawarkrish641@gmail.com>
2026-08-05 10:14:55 +0530

    Update solution: Kth Largest Element in an Array (Medium)

--- Array/kth-largest-element-in-an-array/Solution.java
+++ Array/kth-largest-element-in-an-array/Solution.java
@@ -1,9 +1,8 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int n=nums.length;
         Arrays.sort(nums);
-        System.out.print(nums[n-k]);
-        return nums[n-k];
+        int n=nums.length;
+        int kth=n-k;
+        return nums[kth];
     }
 }
-    
