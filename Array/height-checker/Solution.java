84cf3ff6719fbbb5101b3584613591c9068b862c
Krish Pawar <pawarkrish641@gmail.com>
2026-08-12 09:39:16 +0530

    Add solution: Height Checker (Easy)

---
+++ Array/height-checker/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+       int n=heights.length;
+       int[] sorted=new int[n]; 
+       for(int i=0;i<n;i++){
+        sorted[i]=heights[i];
+            }
+       Arrays.sort(sorted);
+       int numberofindices=0;
+       for(int i=0;i<n;i++){
+        if(heights[i] != sorted[i]){
+            numberofindices++;
+            }
+        }
+        return numberofindices;
+    }
+}
