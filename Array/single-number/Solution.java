4739b77e9dd8784a1de8b3ee34ec17487179a8cb
Krish Pawar <pawarkrish641@gmail.com>
2026-08-15 23:48:49 +0530

    Update solution: Single Number (Easy)

--- Array/single-number/Solution.java
+++ Array/single-number/Solution.java
@@ -1,16 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            int count =0;
-            for(int j=0;j<n;j++){
-            if(nums[i]==nums[j]){
-                count ++;
-                }            
-            }
-            if(count==1)
-            return nums[i];
+
+        int ans = 0;
+
+        for(int num : nums) {
+            ans ^= num;
         }
-        return -1;
+
+        return ans;
     }
 }
