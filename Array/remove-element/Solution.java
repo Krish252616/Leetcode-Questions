24dceda37d705e04262abdac92ddb7e3c8489598
Krish Pawar <pawarkrish641@gmail.com>
2026-08-18 22:55:30 +0530

    Add solution: Remove Element (Easy)

---
+++ Array/remove-element/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count=0;
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            count++;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==val){
+                count--;
+            }
+            else if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return count;
+    }
+}
