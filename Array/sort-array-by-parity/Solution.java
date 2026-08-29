fccb337b11f36b9dd9d2fc1772cc22034cb2d1eb
Krish Pawar <pawarkrish641@gmail.com>
2026-08-29 23:32:58 +0530

    Add solution: Sort Array By Parity (Easy)

---
+++ Array/sort-array-by-parity/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int k=0;
+        int l=nums.length-1;
+        while(k < l){
+            if(nums[k] % 2 != 0 && nums[l] % 2 == 0 ){
+                int temp=nums[k];
+                nums[k]=nums[l];
+                nums[l]=temp;
+                k++;
+                l--;
+            }
+            else if(nums[k] % 2 == 0){
+                k++;
+            }
+            else if(nums[l] % 2 != 0){
+                l--;
+            }
+        }
+        return nums;
+    }
+}
